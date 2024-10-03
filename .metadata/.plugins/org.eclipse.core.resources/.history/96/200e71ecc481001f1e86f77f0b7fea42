906f2104f3e2f6912c038271b1b6def6cd4ceab3
nahuelfsotelo <nahuelsotelo06@gmail.com>
2024-10-03 17:50:46 -0300

    dirverGPIO a medias

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/96/200e71ecc481001f1e86f77f0b7fea42
@@ -0,0 +1,20 @@
+/*
+ * API_GPIO.h
+ *
+ *  Created on               : Oct 3, 2024
+ *      Author               : Nahue
+ *  Funtion of driver        : Driver que contiene funciones para manejo de puertos GPIO que utiliza funciones de la HAL
+ *                             de STM32 nucleo f4xx
+ */
+
+#ifndef API_INC_API_GPIO_H_
+#define API_INC_API_GPIO_H_
+/*Exported types*******************************************************************************/
+ typedef uint16_t led_t; /* importante que el tipo se uint16_t, si no, no funciona LD3_Pin */
+
+
+
+ /*Exported functions prototypes **************************************************************/
+ void MX_GPIO_Init(void);
+
+#endif /* API_INC_API_GPIO_H_ */
--- Ejemplo04_Driver_GPIO/Debug/Core/Startup/subdir.mk
+++ Ejemplo04_Driver_GPIO/Debug/Core/Startup/subdir.mk
@@ -16,7 +16,7 @@ S_DEPS += \
 
 # Each subdirectory must supply rules for building sources it contributes
 Core/Startup/%.o: ../Core/Startup/%.s Core/Startup/subdir.mk
-	arm-none-eabi-gcc -mcpu=cortex-m4 -g3 -DDEBUG -c -x assembler-with-cpp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@" "$<"
+	arm-none-eabi-gcc -mcpu=cortex-m4 -g3 -DDEBUG -c -I"E:/tecnicas digitales 2/trabajos practicos/Ejemplo04_Driver_GPIO/Drivers/API/Inc" -I"E:/tecnicas digitales 2/trabajos practicos/Ejemplo04_Driver_GPIO/Drivers/API/Src" -I"E:/tecnicas digitales 2/trabajos practicos/Ejemplo04_Driver_GPIO/Drivers/API" -x assembler-with-cpp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@" "$<"
 
 clean: clean-Core-2f-Startup
 
